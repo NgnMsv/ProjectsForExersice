7390134d5a7277edc1a30f61d808e98304601fc0
Negin mousavi <ngn.m2000@gmail.com>
2020-08-07 00:04:32 +0430

    random colors generated

---
+++ day8/Color Game/app.js
@@ -0,0 +1,65 @@
+var colors = generateRandomColors(6);
+var pickedColor = pickedColor();
+var squares = document.querySelectorAll('.square');
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.getElementById('message');
+
+colorDisplay.textContent = pickedColor;
+for (i = 0; i < squares.length; i++) {
+    //add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+    //add eventListeners to each square
+    squares[i].addEventListener('click', function () {
+        //grab color of picked square
+        var clickedColor = this.style.backgroundColor;
+        //compare to the pickedColor
+        if (clickedColor === pickedColor) {
+            //found the color
+            messageDisplay.textContent = 'Correct'
+            //change all of the squares color to the answer color
+            changeColors(clickedColor);
+        } else {
+            //wrong choice
+            this.style.backgroundColor = 'black'
+            messageDisplay.textContent = 'Try Again'
+        }
+    });
+}
+
+function changeColors(color) {
+    //loop through the squares
+    for (i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+    //change each color to the given color
+}
+
+function pickedColor() {
+    var random = Math.floor(Math.random()* colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(number) {
+    //make an array
+    var arr = [];
+    //add num random colors
+    //repeat num times
+    for (i = 0; i < number; i++) {
+    //get random colors and push to the arr
+        arr.push(randomColor());
+    }
+    //return the array
+    return arr;
+}
+
+function randomColor() {
+    //pick a 'red' from 0 -255
+    var r = Math.floor((Math.random()*255 )+ 1)
+    //pick a 'green' from 0 -255
+    var g = Math.floor((Math.random()*255 )+ 1)
+    //pick a 'blue' from 0 -255
+    var b = Math.floor((Math.random()*255 )+ 1)
+
+    return 'rgb('+r+', '+g+', '+b+')';
+
+}
